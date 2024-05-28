68893f32c5a35b39000b5a99c6724aacb6d562d0
jonathan rengius <jonathan.rengius@outlook.com>
2024-05-28 21:03:04 +0200

    complete

--- prepare-prompt/main.go
+++ prepare-prompt/main.go
@@ -1,84 +1,84 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
 	"fmt"
 	"log"
-	"net/http"
-	"os"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/eventbridge"
+	"github.com/aws/aws-sdk-go/service/comprehend"
 )
 
-type Request struct {
-	Text string `json:"text"`
+type InputEvent struct {
+	Detail struct {
+		Text  string `json:"text"`
+		S3Key string `json:"s3Key"`
+		Bedrock bool `json:"bedrock"`
+	} `json:"detail"`
 }
 
-type EventDetail struct {
+type OutputEvent struct {
 	Prompt string `json:"prompt"`
+	S3Key  string `json:"s3Key"`
+	Bedrock bool `json:"bedrock"`
 }
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	eventBusName := os.Getenv("EVENT_BUS_NAME")
-	if eventBusName == "" {
-		log.Println("Error: EVENT_BUS_NAME environment variable is not set")
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusBadRequest,
-			Body:       fmt.Sprintln("missing EVENT_BUS_NAME"),
-		}, nil
+func getTopSentiment(sentimentScores *comprehend.SentimentScore) string {
+	sentiments := map[string]float64{
+		"Positive": *sentimentScores.Positive,
+		"Negative": *sentimentScores.Negative,
+		"Neutral":  *sentimentScores.Neutral,
+		"Mixed":    *sentimentScores.Mixed,
 	}
-	var req Request
-	err := json.Unmarshal([]byte(request.Body), &req)
+
+	var topSentiment string
+	var maxScore float64
+
+	for sentiment, score := range sentiments {
+		if score > maxScore {
+			maxScore = score
+			topSentiment = sentiment
+		}
+	}
+
+	switch topSentiment {
+	case "Positive":
+		topSentiment = "happy"
+	case "Negative":
+		topSentiment = "sad"
+	case "Neutral":
+		fallthrough
+	case "Mixed":
+		topSentiment = "neutral"
+	}
+
+	return topSentiment
+}
+
+func handler(ctx context.Context, event InputEvent) (OutputEvent, error) {
+	svc := comprehend.New(session.Must(session.NewSession(&aws.Config{
+		Region: aws.String("eu-west-1"),
+	})))
+	log.Println("Detecting sentiment for text:", event.Detail.Text)
+	sentimentParams := &comprehend.DetectSentimentInput{
+		Text:         aws.String(event.Detail.Text),
+		LanguageCode: aws.String("en"),
+	}
+
+	sentimentResult, err := svc.DetectSentiment(sentimentParams)
 	if err != nil {
-		log.Println("Error unmarshalling request:", err)
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusBadRequest,
-			Body:       fmt.Sprintf("Invalid request: %s", err.Error()),
-		}, nil
+		return OutputEvent{}, fmt.Errorf("failed to detect sentiment: %w", err)
 	}
 
-	prompt := fmt.Sprintf("Generate an image based on the following text: %s", req.Text)
-	eventDetail := EventDetail{Prompt: prompt}
+	topSentiment := getTopSentiment(sentimentResult.SentimentScore)
 
-	detailJson, err := json.Marshal(eventDetail)
-	if err != nil {
-		log.Println("Error marshalling event detail:", err)
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusInternalServerError,
-			Body:       fmt.Sprintf("Error preparing event detail: %s", err.Error()),
-		}, nil
-	}
+	prompt := fmt.Sprintf("Generate a %s image based on the following text: %s", topSentiment, event.Detail.Text)
 
-	sesh := session.Must(session.NewSession())
-	eb := eventbridge.New(sesh)
+	outputEvent := OutputEvent{Prompt: prompt, S3Key: event.Detail.S3Key, Bedrock: event.Detail.Bedrock}
 
-	event := &eventbridge.PutEventsRequestEntry{
-		Source:       aws.String("prepare_prompt_function"),
-		DetailType:   aws.String("promptSubmitted"),
-		Detail:       aws.String(string(detailJson)),
-		EventBusName: aws.String(eventBusName),
-	}
-
-	_, err = eb.PutEvents(&eventbridge.PutEventsInput{
-		Entries: []*eventbridge.PutEventsRequestEntry{event},
-	})
-
-	if err != nil {
-		log.Println("Error putting event to EventBridge:", err)
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusInternalServerError,
-			Body:       fmt.Sprintf("Error sending event to EventBridge: %s", err.Error()),
-		}, nil
-	}
-
-	return events.APIGatewayProxyResponse{
-		StatusCode: http.StatusOK,
-		Body:       `{"message": "Event sent to EventBridge"}`,
-	}, nil
+	return outputEvent, nil
 }
 
 func main() {
